6f7af52dfbd7539d9b2da3338a253d705a0a09fe
Jack Smith-Hocking <jacksmithhocking.work@gmail.com>
2025-04-09 11:41:14 +0930

    Fixed incorrect namespace in InvalidValue

--- Editor/Windows/TreeStyleProject/InvalidValue.cs
+++ Editor/Windows/TreeStyleProject/InvalidValue.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Sirenix.OdinInspector.Editor;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace LordSheo.Editor.Windows.TSP

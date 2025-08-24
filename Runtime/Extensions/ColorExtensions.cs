5dfa54b499444777e861d4f3fe38ae6e7b368c09
Jack Smith-Hocking <hockingjack818@gmail.com>
2025-08-24 16:17:09 +0930

    Added ColorExtensions

---
+++ Runtime/Extensions/ColorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace LordSheo
+{
+	public static class ColorExtensions
+	{
+		public static Color WithR(this Color color, float val)
+		{
+			color.r = val;
+			return color;
+		}
+
+		public static Color WithG(this Color color, float val)
+		{
+			color.g = val;
+			return color;
+		}
+
+		public static Color WithB(this Color color, float val)
+		{
+			color.b = val;
+			return color;
+		}
+
+		public static Color WithA(this Color color, float val)
+		{
+			color.a = val;
+			return color;
+		}
+
+		public static Color Mult(this Color color, float val)
+		{
+			return color * val;
+		}
+	}
+}

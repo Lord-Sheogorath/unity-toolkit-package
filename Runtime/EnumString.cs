d6c39012480e95e684a9b44219990046cccfc17a
Jack Smith-Hocking <hockingjack818@gmail.com>
2025-09-06 14:10:33 +0930

    Added EnumString

---
+++ Runtime/EnumString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LordSheo
+{
+	public struct EnumString
+	{
+		public string value;
+
+		public static implicit operator EnumString(string val)
+		{
+			return new()
+			{
+				value = val,
+			};
+		}
+
+		public static implicit operator EnumString(Enum val)
+		{
+			return new()
+			{
+				value = val.ToString()
+			};
+		}
+
+		public static implicit operator string(EnumString val)
+		{
+			return val.value;
+		}
+	}
+}
